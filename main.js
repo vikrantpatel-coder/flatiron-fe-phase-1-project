fc3df2788fc90d2fd57b42083059ce72ea93050f
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-07-28 13:57:12 -0400

    Cocktails Select appended

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const drinkSelect = document.querySelector("#drinks");
+const cocktailSelect = document.querySelector("#cocktails");
 
 getDrinks();
 
 function getDrinks() {
-  fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
     .then((r) => r.json())
-    .then((drinks) => renderDrinksOptions(drinks))
+    .then((drinks) => renderDrinkOptions(drinks.drinks))
     .catch();
 }
 
-function renderDrinksOptions(drinks) {}
+function renderDrinkOptions(drinks) {
+  drinks.forEach((drinks) => {
+    const option = document.createElement("option");
+    option.value = drinks.strDrink;
+    option.textContent = drinks.strDrink;
+    cocktailSelect.append(option);
+  });
+}
