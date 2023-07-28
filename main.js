e7ef8af50a48f5ec9ec256cb6accaecc51356ac2
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-07-28 11:48:59 -0400

    fetch request made

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const drinkSelect = document.querySelector("#drinks");
 
+getDrinks();
+
+function getDrinks() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+    .then((r) => r.json())
+    .then((drinks) => renderDrinksOptions(drinks))
+    .catch();
+}
+
+function renderDrinksOptions(drinks) {}
