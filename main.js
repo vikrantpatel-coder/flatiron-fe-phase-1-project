e418573720972d03c89e5644e6ca70e598725450
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-08-02 17:20:06 -0400

    main js update

--- main.js
+++ main.js
@@ -7,35 +7,35 @@ const drinkDetailsSection = document.querySelector(".drink-details-section");
 const drinkDetailsContainer = document.querySelector(
   ".drink-details-container"
 );
-const welcomeSection = document.querySelector(".welcome");
-const mainTitle = document.querySelector(".main-title");
+const welcomeSection = document.querySelector(".welcome"); //Welcome message
+const mainTitle = document.querySelector(".main-title"); ///'Bottoms Up!' title
 
 getCategories();
 getAlcohol();
 getGlass();
 
-//drink categories dropdown menu
+//drink categories fetch request
 function getCategories() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
     .then((r) => r.json())
     .then((categories) => renderCategoriesOptions(categories.drinks))
     .catch((error) => alert(error));
 }
-//cocktail alcohol base drink dropdown
+//cocktail (alcohol base) fetch request
 function getAlcohol() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
     .then((r) => r.json())
     .then((categories) => renderAlcoholOptions(categories.drinks))
     .catch((error) => alert(error));
 }
-//drink bu glass type dropdown
+//drink by glass type fetch request
 function getGlass() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
     .then((r) => r.json())
     .then((glass) => renderGlassOptions(glass.drinks))
     .catch((error) => alert(error));
 }
-//rendering Categories list into select categories dropdown
+//rendering Categories list into select categories <option>
 function renderCategoriesOptions(categories) {
   categories.forEach((category) => {
     const option = document.createElement("option");
@@ -44,7 +44,7 @@ function renderCategoriesOptions(categories) {
     categorySelect.append(option);
   });
 }
-//rendering alcoholic/non-alcoholic category list dropdown
+//rendering alcoholic/non-alcoholic category list <option>
 function renderAlcoholOptions(categories) {
   categories.forEach((category) => {
     const option = document.createElement("option");
@@ -53,7 +53,7 @@ function renderAlcoholOptions(categories) {
     alcoholSelect.append(option);
   });
 }
-//rendering glass types dropdown
+//rendering glass types <option>
 function renderGlassOptions(glass) {
   glass.forEach((glass) => {
     const option = document.createElement("option");
@@ -62,13 +62,14 @@ function renderGlassOptions(glass) {
     glassSelect.append(option);
   });
 }
-//eventlistners
+//eventlistner change event
 categorySelect.addEventListener("change", getDrinksByCategories);
 alcoholSelect.addEventListener("change", getDrinksByAlcohol);
 glassSelect.addEventListener("change", getDrinksByGlass);
+//eventlistner click event when the main tile is clicked
 mainTitle.addEventListener("click", showWelcome);
 
-//categories
+//get categories - change event
 function getDrinksByCategories(e) {
   const category = e.target.value;
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`)
@@ -76,7 +77,7 @@ function getDrinksByCategories(e) {
     .then((drinks) => renderAllDrinks(drinks.drinks))
     .catch((error) => alert(error));
 }
-//alcohol
+//get alcohol(beverage base) - change event
 function getDrinksByAlcohol(e) {
   const alcohol = e.target.value;
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
@@ -84,7 +85,7 @@ function getDrinksByAlcohol(e) {
     .then((drinks) => renderAllDrinks(drinks.drinks))
     .catch((error) => alert(error));
 }
-//glass
+//get glass type - change event
 function getDrinksByGlass(e) {
   const glass = e.target.value;
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${glass}`)
@@ -96,7 +97,9 @@ function getDrinksByGlass(e) {
 function renderAllDrinks(drinks) {
   welcomeSection.style.display = "none";
   drinkDetailsContainer.style.display = "none";
+  //grid layout
   drinkContainer.style.display = "grid";
+  //replaceing new children - drinks
   drinkContainer.replaceChildren();
   selectionH1.textContent =
     alcoholSelect.value || categorySelect.value || glassSelect.value;
@@ -109,7 +112,7 @@ function renderAllDrinks(drinks) {
 }
 
 function renderDrinkCard(drinks) {
-  //img
+  //img <div>
   const { strDrink, strDrinkThumb, idDrink } = drinks;
   const cardDiv = document.createElement("div");
   cardDiv.classList.add("card");
@@ -117,7 +120,7 @@ function renderDrinkCard(drinks) {
   const image = document.createElement("img");
   image.src = strDrinkThumb;
 
-  //Drink title
+  //Drink title <h3>
   const drinkTitleDiv = document.createElement("div");
   drinkTitleDiv.classList.add("drink-title");
   const title = document.createElement("h3");
@@ -127,7 +130,7 @@ function renderDrinkCard(drinks) {
   cardDiv.append(image, drinkTitleDiv);
   drinkContainer.append(cardDiv);
 }
-//drink details
+//drink details for all the drinks by id
 function getDrinkDetails(e, idDrink) {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`)
     .then((r) => r.json())
@@ -136,7 +139,9 @@ function getDrinkDetails(e, idDrink) {
 }
 function renderDrinkDetails(drinkDetails) {
   welcomeSection.style.display = "none";
+  //grid layout
   drinkDetailsContainer.style.display = "grid";
+  //replaceing new children text node
   drinkContainer.replaceChildren();
   selectionH1.textContent = "";
 
@@ -153,7 +158,7 @@ function renderDrinkDetails(drinkDetails) {
         measure = "";
         continue;
       }
-      //if ingredient and measure defined
+      //if ingredient and measure is defined trim down empty spaces.
       let ingredientString =
         (measure ? measure.trim() : "") +
         " " +
@@ -166,7 +171,6 @@ function renderDrinkDetails(drinkDetails) {
 
   const {
     strDrink: drink,
-    strAlcoholic: alcoholic,
     strCategory: category,
     strDrinkThumb: image,
     strInstructions: directions,
@@ -176,6 +180,7 @@ function renderDrinkDetails(drinkDetails) {
   const title = document.createElement("p");
   title.textContent = drink;
   let titleArea = document.querySelector(".drink-details-title");
+  //replaceing new children text title
   titleArea.replaceChildren();
   titleArea.append(title);
 
@@ -183,9 +188,11 @@ function renderDrinkDetails(drinkDetails) {
   const drinkImage = document.createElement("img");
   drinkImage.src = image;
   drinkImage.alt = `Image for ${drink} `;
+  //replaceing new children img
   imageArea.replaceChildren();
   imageArea.append(drinkImage);
 
+  // ingredients <p>
   const ingredients = parseIngredients(drinkDetails);
   const ingredientPs = ingredients.map((ingredient) => {
     const ingredientP = document.createElement("p");
@@ -193,35 +200,44 @@ function renderDrinkDetails(drinkDetails) {
     return ingredientP;
   });
 
-  // ingredients content
+  // ingredients content <h3>
   const ingredientsArea = document.querySelector(".drink-details-ingredients");
   const ingredientsTitle = document.createElement("h3");
   ingredientsTitle.textContent = "Ingredients";
+  //underline text styling
   ingredientsTitle.style.textDecoration = "underline";
+  //replaceing new children text node ingredients
   ingredientsArea.replaceChildren();
   ingredientsArea.append(ingredientsTitle, ...ingredientPs);
 
-  // directions content
+  // directions content <h3>
   const directionsArea = document.querySelector(".drink-details-directions");
   const directionsTitle = document.createElement("h3");
   directionsTitle.textContent = "Directions";
+  //underline text styling
   directionsTitle.style.textDecoration = "underline";
+  //directions <p>
   const directionsP = document.createElement("p");
+  //replaceing new children text node directions
   directionsArea.replaceChildren();
   directionsP.textContent = directions;
   directionsArea.append(directionsTitle, directionsP);
 
-  // preparation video hyperlink
+  // preparation video hyperlink <a>
   const youTubeLinkATag = document.createElement("a");
   youTubeLinkATag.href = videoLink;
   youTubeLinkATag.target = "_blank";
   youTubeLinkATag.text = `How to make ${drink} on YouTube.`;
+  //drink categories elements <p>
   const drinkCategory = document.createElement("p");
   drinkCategory.textContent = `(Drink: ${drink}, Category: ${category})`;
   const resourcesArea = document.querySelector(".drink-details-resources");
+  //replaceing new children text node video links
   resourcesArea.replaceChildren();
   resourcesArea.append(youTubeLinkATag, drinkCategory);
 }
+
+// main title click function brings back to welcome message page
 function showWelcome() {
   drinkDetailsContainer.style.display = "none";
   drinkContainer.style.display = "none";
